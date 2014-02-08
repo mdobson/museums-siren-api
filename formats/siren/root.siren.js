cd5af0da379802f00ab41189e4053f16170cbf54
Matt Dobson <mdobson4@gmail.com>
2014-02-08 14:03:03 -0500

    Added action for hopping down into the museums collection with filtering.

--- formats/siren/root.siren.js
+++ formats/siren/root.siren.js
@@ -2,9 +2,25 @@ module.exports = function(model) {
   var entity = {
     class: ['root']
   };
+
+  entity.actions = model.urls.map(function(resource) {
+    var action = {
+      'name':'get-'+resource.name,
+      'title':'Get '+resource.name,
+      'method':'GET',
+      'href':resource.selfUrl,
+      'fields': [
+        { 'name':'query', 'type':'text' },
+        { 'name':'limit', 'type':'number' }
+      ]
+    };
+
+    return action;
+  });
+
   entity.links = model.urls.map(function(resource) {
       var link = {
-        rel: [resource.name],
+        rel: [resource.name, 'collection'],
         href: resource.selfUrl
       };
 
